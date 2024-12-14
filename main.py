886daa9dc0ce724d283840daf4341c3390cdc6db
its-manas-01 <ghoshmanas309@gmail.com>
2024-12-14 13:00:55 +0530

    bot

--- main.py
+++ main.py
@@ -235,8 +235,24 @@ def mashlemagicandmuscles(update, context):
             Episode 8:  https://www.udlinks.com/AWqcyX0
             Episode 9:  https://www.udlinks.com/mFtD2IE
             Episode 10: https://en.mrproblogger.com/grolTV
-            Episode 11: available very soon :
-            Episode 12: available very soon :
+            Episode 11: https://www.udlinks.com/ntyx
+            Episode 12: https://en.mrproblogger.com/UA9RcSy
+        season 2:
+            Episode 1:  https://www.udlinks.com/VgYk
+            Episode 2:  https://en.mrproblogger.com/OZn7
+            Episode 3:  https://www.udlinks.com/wKldtJV
+            Episode 4:  https://en.mrproblogger.com/Bxcc4Rbi
+            Episode 5:  https://www.udlinks.com/hQ6BN
+            Episode 6:  https://en.mrproblogger.com/9baR
+            Episode 7:  https://www.udlinks.com/58vZzs
+            Episode 8:  https://www.udlinks.com/xiG0F
+            Episode 9:  https://www.udlinks.com/Lauta4
+            Episode 10: https://en.mrproblogger.com/VI39eO
+            Episode 11: https://en.mrproblogger.com/AsW3t2k
+            Episode 12: https://www.udlinks.com/5Y2qys
+        Mashle Magic and Mscles has been completed.
+        Please wait for next season.
+        If you want to watch other anime then click here 👉 /anime . 
         """
         )   
 
