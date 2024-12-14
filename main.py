0c711c510e3669efffa4189fa9edd4398b6302c9
its-manas-01 <ghoshmanas309@gmail.com>
2024-12-14 12:40:45 +0530

    bot

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def start(update, context):
     update.message.reply_text(
         """
         Welcome to Anime Hub.
-        please type /anime for choicing your anime.
+        please type /anime for choosing your anime.
         """
         )
 
@@ -31,28 +31,28 @@ def anime(update, context):
     update.message.reply_text(
         """
         Anime Series:
-        /Naruto->1.Naruto
-        /NarutoShippuden->2.Naruto Shippuden
-        /OnePunchMan->3.  One Punch Man
-        /DeathNote->4.  Death Note
-        /Bleach->5.  Bleach
-        /DragonBallZ->6.  Dragon Ball Z
-        /TowerofGod->7.  Tower of God
-        /Tokyorevengers->8.  Tokyo  Revenge
-        /BlueLock->9.  Blue Lock
-        /ThatTimeIgotReincarnatedasASlime->10. That Time I Got Reincarnated as a Slime
-        /Blackclover->11. Black clover
-        /TrueBeauty->12. True Beauty
-        /NoLongerAllowedinAnotherWorld->13. No Longer Allowed in Another World
-        /WDNRMITW->14. Why Does Nobody Remember Me in This World
-        /SpyXFamily->15. Spy X Family
-        /TheImmortalKing->16. The Immortal King
-        /DrStone->17. Dr. Stone
-        /365daysToTheWedding->18. 365 days to  the Wedding
-        /LogHorizen->19. Log Horizen
-        /YouArMsServant->20. You ar Ms. Servant
-        /DADADAN->21. DA DA DAN
-        /soloLeveling->22. Solo Leveling
+        /Naruto->Naruto
+        /NarutoShippuden->Naruto Shippuden
+        /OnePunchMan->One Punch Man
+        /DeathNote->Death Note
+        /Bleach->Bleach
+        /DragonBallZ->Dragon Ball Z
+        /TowerofGod->Tower of God
+        /Tokyorevengers->Tokyo  Revenge
+        /BlueLock->Blue Lock
+        /ThatTimeIgotReincarnatedasASlime->That Time I Got Reincarnated as a Slime
+        /Blackclover->Black clover
+        /TrueBeauty->True Beauty
+        /NoLongerAllowedinAnotherWorld->No Longer Allowed in Another World
+        /WDNRMITW->Why Does Nobody Remember Me in This World
+        /SpyXFamily->Spy X Family
+        /TheImmortalKing->The Immortal King
+        /DrStone->Dr. Stone
+        /365daysToTheWedding->365 days to  the Wedding
+        /LogHorizen->Log Horizen
+        /YouArMsServant->You ar Ms. Servant
+        /DADADAN->DA DA DAN
+        /soloLeveling->Solo Leveling
         /mashlemagicandmuscles->Mashle Magic and Mscles
         Anime Movies:
         /Iwanttoeatyourpancreas->I want to Eat Your Pancreas 
@@ -61,89 +61,164 @@ def anime(update, context):
 
 
 def Naruto(update, context):
-    update.message.reply_text("Naruto : https://shrinkme.ink/UGjkA")
+    update.message.reply_text(
+        """
+        Naruto :
+        """
+        )
 
 
 def NarutoShippuden(update, context):
-    update.message.reply_text(" Naruto  Shippuden : https://shrinkme.ink/a71PD")
+    update.message.reply_text(
+        """
+        Naruto  Shippuden : 
+        """
+        )
 
 
 def OnePunchMan(update, context):
-    update.message.reply_text(" One Punch Man :https://en.mrproblogger.com/sF8t")
+    update.message.reply_text(
+        """
+        One Punch Man :
+        """
+        )
 
 
 def DeathNote(update, context):
-    update.message.reply_text("Death Note : https://en.mrproblogger.com/QBzRh")
+    update.message.reply_text(
+        """
+        Death Note :
+        """
+        )
 
 
 def Bleach(update, context):
-    update.message.reply_text(" Bleach : https://en.shrinke.me/G8zS")
+    update.message.reply_text(
+        """
+        Bleach :
+        """
+        )
 
 def TowerofGod(update, context):
-    update.message.reply_text(" Tower   of God : https://shrinkme.ink/jJJxQx")
+    update.message.reply_text(
+        """ 
+        Tower of God :
+        """
+        )
 
 
 def Tokyorevengers(update, context):
-    update.message.reply_text(" Tokyo  Revenge :https://shrinkme.ink/RtbcOeAr")
+    update.message.reply_text(
+        """
+        Tokyo  Revenge :
+        """
+        )
     
 def BlueLock(update, context):
-    update.message.reply_text(" Blue  Lock : https://shrinkme.ink/w801")
+    update.message.reply_text(
+        """ 
+        Blue  Lock :
+        """
+        )
     
 def ThatTimeIgotReincarnatedasASlime(update, context):
-    update.message.reply_text("This anime will be avlible in next week.")
+    update.message.reply_text(
+        """
+        This anime will be avlible in next week.
+        """
+        )
     
 
 def DragonBallZ(update, context):
     update.message.reply_text("This anime will be avlible in next week.")
 
 def Blackclover(update, context):
-    update.message.reply_text("This anime will be avlible in next week.")
+    update.message.reply_text(
+        """
+        This anime will be avlible in next week.
+        """
+        )
     
 
 def TrueBeauty(update, context):
-    update.message.reply_text("This anime will be avlible in next week.")
+    update.message.reply_text(
+        """
+        This anime will be avlible in next week.
+        """
+        )
     
     
 def NoLongerAllowedinAnotherWorld(update, context):
-    update.message.reply_text("This anime will be avlible in next week.")
+    update.message.reply_text(
+        """
+        This anime will be avlible in next week.
+        """
+        )
     
     
 def WDNRMITW(update,context):
-    update.message.reply_text("This anime will be avlible in next week.")
-    
+    update.message.reply_text(
+        """
+        This anime will be avlible in next week.
+        """
+        )
     
 def SpyXFamily(update, context):
-    update.message.reply_text("This anime will be avlible in next week.")
-    
+    update.message.reply_text(
+        """
+        This anime will be avlible in next week.
+        """
+        )
     
 def TheImmortalKing(update, context):
-    update.message.reply_text("This anime will be avlible in next week.")
-    
+    update.message.reply_text(
+        """
+        This anime will be avlible in next week.
+        """
+        )
     
 def DrStone(update, context):
-    update.message.reply_text("This anime will be avlible in next week.")
-    
+    update.message.reply_text(
+        """
+        This anime will be avlible in next week.
+        """
+        )
     
 def LogHorizen(update, context):
-    update.message.reply_text("This anime will be avlible in next week.")
-    
+    update.message.reply_text(
+        """
+        This anime will be avlible in next week.
+        """
+        )
     
 def YouArMsServant(update, context):
-    update.message.reply_text("This anime will be avlible in next week.")
-    
+    update.message.reply_text(
+        """
+        This anime will be avlible in next week.
+        """
+        )
     
 def soloLeveling(update, context):
-    update.message.reply_text("This anime will be avlible in next week.")
-    
+    update.message.reply_text(
+        """
+        This anime will be avlible in next week.
+        """
+        )
     
     
 def daystotheWedding(update,context):
-    update.message.reply_text("This anime will be avlible in next week.")
-    
+    update.message.reply_text(
+        """
+        This anime will be avlible in next week.
+        """
+        )
     
 def DADADAN(update,context):
-    update.message.reply_text("This anime will be avlible in next week.")
-    
+    update.message.reply_text(
+        """
+        This anime will be avlible in next week.
+        """
+        )
  
 def mashlemagicandmuscles(update, context):
     update.message.reply_text(
@@ -167,8 +242,11 @@ def mashlemagicandmuscles(update, context):
 
 
 def Iwanttoeatyourpancreas(update, context):
-      update.message.reply_text("This anime will be avlible in next week.")
-    
+    update.message.reply_text(
+        """
+        This anime will be avlible in next week.
+        """
+        )
 dispatcher.add_handler(CommandHandler('start', start))
 dispatcher.add_handler(CommandHandler('help', help))
 dispatcher.add_handler(CommandHandler('anime', anime))
