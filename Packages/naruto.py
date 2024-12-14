4acd93e306fc19d1b21d5f115741b674e67431bc
its-manas-01 <ghoshmanas309@gmail.com>
2024-12-14 19:48:32 +0530

    adding packages

--- Packages/naruto.py
+++ Packages/naruto.py
@@ -4,8 +4,9 @@ class ognaruto:
     def __init__(self,Token):
         self.updater = Updater(Token)
         self.dispatcher = self.updater.dispatcher
-    def Sesaon1(self,update, context):
-        update.message.reply_text("Naruto : https://shrinkme.ink/UGjkA")       
+        
+    # def Sesaon1(self,update, context):
+    #     update.message.reply_text("Naruto : https://shrinkme.ink/UGjkA")       
         
     def naruto1(self,update, context):
         update.message.reply_text(
