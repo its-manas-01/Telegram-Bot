94a276c0720d2974f5a377c58128f852f9c4e55e
its-sagar-01 <majhi01sagar@gmail.com>
2024-12-15 21:05:54 +0530

    Blu Lock

--- Packages/bluelock.py
+++ Packages/bluelock.py
@@ -13,3 +13,19 @@ class bluelock:
             Episode 1:
         """
         )
+
+    def Season2(self,update,context):
+        update.massage.reply_test(
+         """
+        Season 2:
+            Episode 1:  https://www.udlinks.com/ZQuO3j
+            Episode 2:  https://www.udlinks.com/ZR76JfpC
+            Episode 3:  https://www.udlinks.com/VrlXDF
+            Episode 4:  https://www.udlinks.com/mWDxp
+            Episode 5:  https://www.udlinks.com/IM28z
+            Episode 6:  https://www.udlinks.com/KOUnrSwB
+            Episode 7:  https://www.udlinks.com/eYWW
+            Episode 8:  https://www.udlinks.com/GG6jL
+            
+        """
+        )
