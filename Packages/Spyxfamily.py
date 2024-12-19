baf3e524641170c8af62a779d97824fe5393f2c0
its-manas-01 <ghoshmanas309@gmail.com>
2024-12-19 08:18:47 +0530

    spy x family complete

--- Packages/Spyxfamily.py
+++ Packages/Spyxfamily.py
@@ -10,30 +10,51 @@ class Spyxfamily:
        update.message.reply_text(
         """
         Season 1:
-            Episode 1:   https://www.udlinks.com/mrfb
-            Episode 2:   https://www.udlinks.com/Qmmu0w
-            Episode 3:   https://www.udlinks.com/yvnM
-            Episode 4:   
-            Episode 5:   
-            Episode 6:   
-            Episode 7:   
-            Episode 8:   
-            Episode 9:   
-            Episode 10:   
-            Episode 11:   
-            Episode 12:   
-            Episode 13:   
-            Episode 14:   
-            Episode 15:   
-            Episode 16:   
-            Episode 17:   
-            Episode 18:   
-            Episode 19:   
-            Episode 20:   
-            Episode 21:   
-            
-        
-
+            Episode 1:  https://www.udlinks.com/mrfb
+            Episode 2:  https://www.udlinks.com/Qmmu0w
+            Episode 3:  https://www.udlinks.com/yvnM
+            Episode 4:  https://www.udlinks.com/yvnM
+            Episode 5:  https://www.udlinks.com/jpLb7uBK
+            Episode 6:  https://www.udlinks.com/KZDZY4HY
+            Episode 7:  https://www.udlinks.com/L7nch
+            Episode 8:  https://www.udlinks.com/wROQXrC
+            Episode 9:  https://www.udlinks.com/FxK7MQrw
+            Episode 10: https://www.udlinks.com/FxK7MQrw
+            Episode 11: https://www.udlinks.com/U7EGx6So
+            Episode 12: https://www.udlinks.com/2dDJ
+            Episode 13: https://www.udlinks.com/tNMgeTD
+            Episode 14: https://www.udlinks.com/eXRUL9r
+            Episode 15: https://www.udlinks.com/Jdi0N6Mi
+            Episode 16: https://www.udlinks.com/LkaWtue
+            Episode 17: https://www.udlinks.com/zc4UJ
+            Episode 18: https://www.udlinks.com/et2fdb
+            Episode 19: https://www.udlinks.com/3bnsU9
+            Episode 20: https://www.udlinks.com/4svlnz
+            Episode 21: https://www.udlinks.com/XuADE
+            Episode 22: https://www.udlinks.com/zhTr
+            Episode 23: https://www.udlinks.com/mCNlB
+            Episode 24: https://www.udlinks.com/zKIw5tQ
+            Episode 25: https://www.udlinks.com/u8hqqdU 
+        """
+        )
+       
+       
+    def Season2(self, update, context):
+        update.message.reply_text(
+        """
+        Season 2:
+            Episode 1:  https://www.udlinks.com/1fa5X
+            Episode 2:  https://www.udlinks.com/fBBmPeMR
+            Episode 3:  https://www.udlinks.com/IiCk
+            Episode 4:  https://www.udlinks.com/gleL
+            Episode 5:  https://www.udlinks.com/l16Xj
+            Episode 6:  https://www.udlinks.com/Xfsud3
+            Episode 7:  https://www.udlinks.com/LBb8
+            Episode 8:  https://www.udlinks.com/RpKTJ3
+            Episode 9:  https://www.udlinks.com/WANovbs
+            Episode 10: https://www.udlinks.com/loBNLF3O
+            Episode 11: https://www.udlinks.com/N5Kf
+            Episode 12: https://www.udlinks.com/Rn3piZ48
         """
         )
        
