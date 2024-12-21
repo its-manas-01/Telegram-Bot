182f708a2fdbb131a2d11c010f40ad1559b9f0cd
its-manas-01 <ghoshmanas309@gmail.com>
2024-12-21 21:09:46 +0530

    immortal king

--- Packages/thedailylifeofimmortalking.py
+++ Packages/thedailylifeofimmortalking.py
@@ -31,52 +31,52 @@ class ThedailylifeofimmortalKing:
         update.message.reply_text(
         """
         season 2:
-            Episode 1:  
-            Episode 2:  
-            Episode 3:  
-            Episode 4:  
-            Episode 5:  
-            Episode 6:  
-            Episode 7:  
-            Episode 8:  
-            Episode 9:  
-            Episode 10: 
-            Episode 11: 
-            Episode 12:
+            Episode 1:  https://www.udlinks.com/G5zy9
+            Episode 2:  https://www.udlinks.com/Cn5b
+            Episode 3:  https://www.udlinks.com/qRIN
+            Episode 4:  https://www.udlinks.com/MEMORFY
+            Episode 5:  https://www.udlinks.com/oH3VilYQ
+            Episode 6:  https://www.udlinks.com/7kYbuYJ
+            Episode 7:  https://www.udlinks.com/efuGJ
+            Episode 8:  https://www.udlinks.com/W73QX
+            Episode 9:  https://www.udlinks.com/4fNbafy
+            Episode 10: https://www.udlinks.com/NRAmHrTK
+            Episode 11: https://www.udlinks.com/XQKofMUT
+            Episode 12: https://www.udlinks.com/olspdQXh
         """
         )   
     def Season3(self,update,context):
         update.message.reply_text(
         """ 
-        season 1: 
-            Episode 1:  
-            Episode 2:  
-            Episode 3:  
-            Episode 4:  
-            Episode 5:  
-            Episode 6:  
-            Episode 7:  
-            Episode 8:  
-            Episode 9:  
-            Episode 10: 
-            Episode 11: 
-            Episode 12: 
+        season 3: 
+            Episode 1:  https://www.udlinks.com/y2m1NZ
+            Episode 2:  https://www.udlinks.com/quPpH
+            Episode 3:  https://www.udlinks.com/4QZ4SQI
+            Episode 4:  https://www.udlinks.com/Rxn4g5
+            Episode 5:  https://www.udlinks.com/M2SAB6U
+            Episode 6:  https://www.udlinks.com/yOqvpbL
+            Episode 7:  https://www.udlinks.com/JBIu1V
+            Episode 8:  https://www.udlinks.com/JafytY
+            Episode 9:  https://www.udlinks.com/Ug74
+            Episode 10: https://www.udlinks.com/Qorj
+            Episode 11: https://www.udlinks.com/DCkyEhqC
+            Episode 12: https://www.udlinks.com/ZtPdeBW
         """
         )
     def Season4(self,update,context):
         update.message.reply_text(
         """ 
-        season 1: 
-            Episode 1:  
-            Episode 2:  
-            Episode 3:  
-            Episode 4:  
-            Episode 5:  
-            Episode 6:  
-            Episode 7:  
-            Episode 8:  
-            Episode 9:  
-            Episode 10: 
-            Episode 11: 
-            Episode 12: 
+        season 4: 
+            Episode 1:  https://www.udlinks.com/9S56P
+            Episode 2:  https://www.udlinks.com/L38UHTx8
+            Episode 3:  https://www.udlinks.com/Eohv1yG
+            Episode 4:  https://www.udlinks.com/fjtK9JxE
+            Episode 5:  https://www.udlinks.com/LigsrH
+            Episode 6:  https://www.udlinks.com/m9qMTG
+            Episode 7:  https://www.udlinks.com/einb9kG9
+            Episode 8:  https://www.udlinks.com/EvB400W9
+            Episode 9:  https://www.udlinks.com/75pK
+            Episode 10: https://www.udlinks.com/HmYkPA
+            Episode 11: https://www.udlinks.com/6C7vB3dd
+            Episode 12: https://www.udlinks.com/HDO1L
         """)
